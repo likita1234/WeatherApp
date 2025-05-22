1fd01d12e226f315dbc80726625243be98e84914
Likita Maharjan <likitaof2015@gmail.com>
2025-05-22 16:16:55 +0100

    Added switch with icon to handle theme change

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -1,16 +1,32 @@
-import React from "react";
-import { Button, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Switch } from "react-native";
 import { useTheme } from "@theme/ThemeContext";
+import { FontAwesome } from "@expo/vector-icons";
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
+  const [isDark, setIsDark] = useState(theme === "dark" ? true : false);
 
+  const toggleValue = () => {
+    toggleTheme();
+    setIsDark((prev) => !prev);
+  };
   return (
     <View style={styles.container}>
-      <Button
-        title={`Switch to ${theme === "light" ? "Dark" : "Light"} Mode`}
-        onPress={toggleTheme}
-      />
+      <View style={styles.subContainer}>
+        <View style={styles.iconContainer}>
+          <FontAwesome
+            name={isDark ? "sun-o" : "moon-o"}
+            size={20}
+            color={isDark ? "orange" : "black"}
+          />
+        </View>
+        <Switch
+          value={isDark}
+          onValueChange={toggleValue}
+          thumbColor={isDark ? "#fff" : "#f4f3f4"}
+        />
+      </View>
     </View>
   );
 };
@@ -18,5 +34,15 @@ const ThemeToggle = () => {
 export default ThemeToggle;
 
 const styles = StyleSheet.create({
-  container: { marginVertical: 10 },
+  container: {
+    alignItems: "flex-end",
+    paddingHorizontal: 10,
+  },
+  subContainer: {
+    flexDirection: "row",
+  },
+  iconContainer: {
+    justifyContent: "center",
+    marginRight: 4,
+  },
 });
